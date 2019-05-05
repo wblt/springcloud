207bab0420c2713941a12ff393e9b3e20b34437e
wblt <940422068@qq.com>
2019-05-05 22:30:53 +0800

    hello

---
+++ springcloud-hello/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
