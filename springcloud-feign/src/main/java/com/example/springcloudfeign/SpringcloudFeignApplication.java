240586fff3f616a11e53475dcf9f740540f32071
wblt <940422068@qq.com>
2019-05-06 12:56:29 +0800

    feigin

---
+++ springcloud-feign/src/main/java/com/example/springcloudfeign/SpringcloudFeignApplication.java
@@ -0,0 +1,18 @@
+package com.example.springcloudfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudFeignApplication.class, args);
+    }
+
+}
